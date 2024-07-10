63622979252c772f24d49724a2244694133f47b6
Yuen Ler Chow <lerchow@gmail.com>
2024-07-10 10:23:23 -0700

    allow diff response types

---
+++ src/ResponseType.ts
@@ -0,0 +1,13 @@
+/**
+ * Response types
+ * @author Yuen Ler Chow
+ */
+enum ResponseType {
+  Text = 'Text',
+  Blob = 'Blob',
+  ArrayBuffer = 'ArrayBuffer',
+  FormData = 'FormData',
+  JSON = 'JSON',
+}
+
+export default ResponseType;
